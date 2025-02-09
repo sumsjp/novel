4b62683edb907853fe307726eda7aa40a9f12475
MingShing Su <mingshing.su@gmail.com>
2025-02-09 09:27:42 +0800

    .

--- code/conv_novels.py
+++ code/conv_novels.py
@@ -1,4 +1,5 @@
 import os
+import re 
 
 ORIG = "../orig"
 DOCS = "../docs"
@@ -8,6 +9,7 @@ PRE_HTML = '''<!DOCTYPE html>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Novels</title>
+    <link rel="stylesheet" href="/docs/novel.css">
 </head>
 <body>
 '''
@@ -16,15 +18,30 @@ POST_HTML = '''</body>
 </html>
 '''
 
+def get_title(fname):
+    with open(fname, "r", encoding="utf-8") as f:
+        content = f.read()
+
+    # Use regex to extract text inside <title>...</title>
+    match = re.search(r"<title>(.*?)</title>", content, re.S)
+    return match.group(1) if match else ""
 
 def create_subject_index(subject):
     index_html = f"{DOCS}/{subject}/index.html"
     os.makedirs(os.path.dirname(index_html), exist_ok=True)
 
+    orig_path = f"{ORIG}/{subject}"
+    docs = sorted(os.listdir(orig_path))
+    docs = [doc[:-4] for doc in docs if doc[-4:] == '.xml']
+
     with open(index_html, "w") as fh:
         fh.write(PRE_HTML)
         fh.write(f'    <h2>{subject}</h2>\n')
         fh.write(f'    <p><a href="../index.html">回到小說總表</a></p>\n')
+        for doc in docs:
+            doc_file = f"{orig_path}/{doc}.xml"
+            doc_title = get_title(doc_file)
+            fh.write(f'    <p><a href="{doc}.html">[{doc}]　{doc_title}</a></p>\n')
         fh.write(POST_HTML)
 
 def create_root_index(dirs):
@@ -40,7 +57,7 @@ def create_root_index(dirs):
         fh.write(POST_HTML)
 
 def main():
-    dirs = os.listdir(ORIG)
+    dirs = sorted(os.listdir(ORIG))
     dirs = [dir for dir in dirs if os.path.isfile(f'{ORIG}/{dir}/001.xml')]
     
     create_root_index(dirs)
